5b17d47b48a9e9efec5b641ae0e5f4295ff0ce97
AzatSatlykov <azatsatlykov@yahoo.com>
2023-09-28 19:10:21 +0500

    1

---
+++ Example3/Program.cs
@@ -0,0 +1,19 @@
+﻿// 
+
+int [] array = {11,21,3,4,15,6,7,18,15};
+
+int n = array.Length;
+int find = 15;
+
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
